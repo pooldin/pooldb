0d0fae28e3ccf5d3491223b43908d518573d6f7b
Brian Oldfield <brian.oldfield@gmail.com>
2012-10-10 11:17:12 -0700

    Updated varchar columns that should be enumerations, corrected community references (community v. campaign).

--- src/pooldb/sql/all.sql
+++ src/pooldb/sql/all.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "user" (
 "id" serial8 NOT NULL,
-"enabled" bool NOT NULL DEFAULT False,
+"enabled" bool NOT NULL DEFAULT True,
 "verified" bool NOT NULL DEFAULT False,
 "username" varchar(40) NOT NULL,
 "password" varchar(255) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE "user" (
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE UNIQUE INDEX "user_username_index" ON "user" ("username");
@@ -21,7 +21,7 @@ CREATE TABLE "user_meta" (
 "create" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE INDEX "user_meda_user_id_index" ON "user_meta" ("user_id" ASC);
@@ -29,7 +29,7 @@ CREATE INDEX "user_meda_user_id_index" ON "user_meta" ("user_id" ASC);
 CREATE TABLE "balance" (
 "id" serial8 NOT NULL,
 "enabled" bool NOT NULL DEFAULT True,
-"type" balance_type NOT NULL,
+"type" balance_type_enum NOT NULL,
 "user_id" serial8,
 "community_id" serial8,
 "currency_id" serial8 NOT NULL,
@@ -37,10 +37,10 @@ CREATE TABLE "balance" (
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
-COMMENT ON COLUMN "balance"."type" IS 'enumerations: {''user'', ''campaign''}';
+COMMENT ON COLUMN "balance"."type" IS 'enumerations: {''user'', ''community''}';
 
 CREATE TABLE "currency" (
 "id" serial8 NOT NULL,
@@ -52,7 +52,7 @@ CREATE TABLE "currency" (
 "sign" varchar(1) NOT NULL,
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE INDEX "balance_number_index" ON "currency" ("number");
@@ -60,15 +60,15 @@ CREATE INDEX "balance_number_index" ON "currency" ("number");
 CREATE TABLE "transaction" (
 "id" uuid NOT NULL,
 "balance_id" serial8,
-"campaign_goal_id" serial8,
+"community_goal_id" serial8,
 "debit" decimal(24,4) NOT NULL,
 "credit" decimal(24,4) NOT NULL,
 "created" timestamptz NOT NULL,
 "remote_ip" cidr NOT NULL,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
-COMMENT ON COLUMN "transaction"."campaign_goal_id" IS 'Used to link a transaction with a specific campaign goal for auditing/analytics purposes.';
+COMMENT ON COLUMN "transaction"."community_goal_id" IS 'Used to link a transaction with a specific community goal for auditing/analytics purposes.';
 
 CREATE TABLE "transfer" (
 "id" uuid NOT NULL,
@@ -78,7 +78,7 @@ CREATE TABLE "transfer" (
 "credit" decimal(24,4) NOT NULL,
 "created" timestamptz NOT NULL,
 "remote_ip" cidr NOT NULL,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "exchange" (
@@ -91,22 +91,22 @@ CREATE TABLE "exchange" (
 "credit" decimal(24,4) NOT NULL,
 "created" timestamptz NOT NULL,
 "remote_ip" cidr NOT NULL,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "external_ledger" (
 "id" uuid NOT NULL,
 "record_id" uuid NOT NULL,
-"record_table" varchar(255) NOT NULL,
+"record_table" record_table_enum NOT NULL,
 "party" varchar(255) NOT NULL,
-"external_reference_number" varchar,
+"external_reference_number" varchar(255),
 "fee_id" serial8,
 "currency_id" serial8 NOT NULL,
 "debit" decimal(24,4) NOT NULL,
 "credit" decimal(24,4) NOT NULL,
 "created" timestamptz NOT NULL,
 "remote_ip" cidr NOT NULL,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "fee" (
@@ -116,7 +116,8 @@ CREATE TABLE "fee" (
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr NOT NULL,
-PRIMARY KEY ("id")
+"enabled" bool NOT NULL DEFAULT True,
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "community" (
@@ -129,7 +130,7 @@ CREATE TABLE "community" (
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "community_goal" (
@@ -137,13 +138,13 @@ CREATE TABLE "community_goal" (
 "enabled" bool NOT NULL DEFAULT True,
 "community_id" serial8 NOT NULL,
 "purchase_id" serial8,
-"type" community_goal_type NOT NULL,
+"type" community_goal_type_enum NOT NULL,
 "start" timestamptz NOT NULL,
 "end" timestamptz NOT NULL,
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "community_goal_meta" (
@@ -155,17 +156,18 @@ CREATE TABLE "community_goal_meta" (
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "community_goal_meta_key" (
-"id" varchar(255) NOT NULL,
+"id" serial8 NOT NULL,
+"key" varchar(255) NOT NULL,
 "enabled" bool NOT NULL,
 "description" text NOT NULL,
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+CONSTRAINT "id" PRIMARY KEY ("id", "key") 
 );
 
 CREATE TABLE "community_association" (
@@ -173,27 +175,25 @@ CREATE TABLE "community_association" (
 "enabled" bool NOT NULL DEFAULT True,
 "community_id" serial8 NOT NULL,
 "user_id" serial8 NOT NULL,
-"role" community_role_type NOT NULL DEFAULT 'participant',
+"role" community_role_enum NOT NULL DEFAULT 'participant',
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "invitee" (
-"id" varchar(255) NOT NULL,
+"id" uuid NOT NULL,
 "enabled" bool NOT NULL DEFAULT True,
 "user_id" serial8,
-"campaign_id" serial8 NOT NULL,
+"community_id" serial8 NOT NULL,
 "email" varchar(255) NOT NULL,
 "accepted" timestamptz,
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
-
-COMMENT ON COLUMN "invitee"."id" IS 'Composed of sha1(email + campaign_id) to uniquely identify an invitation.';
 
 CREATE TABLE "internal_ledger" (
 "id" uuid NOT NULL,
@@ -206,13 +206,13 @@ CREATE TABLE "internal_ledger" (
 "credit" decimal(24,4) NOT NULL,
 "created" timestamptz NOT NULL,
 "remote_ip" cidr NOT NULL,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "purchase" (
 "id" serial8 NOT NULL,
 "name" varchar(255) NOT NULL,
-"description" varchar(255) NOT NULL,
+"description" text NOT NULL,
 "purchase_ledger_id" uuid NOT NULL,
 "fulfilled" bool NOT NULL DEFAULT False,
 "refunded" bool NOT NULL DEFAULT False,
@@ -221,24 +221,24 @@ CREATE TABLE "purchase" (
 "address_two" varchar(255),
 "city" varchar(255),
 "state" varchar(4),
-"country" varchar(32),
-"zip" varchar(255),
+"country" varchar(255),
+"zip" varchar(32),
 "email" varchar(255) NOT NULL,
 "created" timestamptz NOT NULL,
 "modified" timestamptz NOT NULL,
 "remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
 
 CREATE TABLE "user_purchase" (
 "id" serial8 NOT NULL,
 "user_id" serial8 NOT NULL,
 "purchase_id" serial8 NOT NULL,
-"created" timestamptz NOT NULL,
-"modified" timestamptz NOT NULL,
-"remote_ip" cidr,
-PRIMARY KEY ("id")
+PRIMARY KEY ("id") 
 );
+
+COMMENT ON TABLE "user_purchase" IS 'This is unabashedly a bare-bones through table. ''nuff said.';
+
 
 ALTER TABLE "user_meta" ADD CONSTRAINT "a4_user_meta_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 ALTER TABLE "balance" ADD CONSTRAINT "a4_balance_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
@@ -248,12 +248,12 @@ ALTER TABLE "exchange" ADD CONSTRAINT "a4_exchange_balance_id_fk" FOREIGN KEY ("
 ALTER TABLE "transaction" ADD CONSTRAINT "a4_transaction_balance_id_fk" FOREIGN KEY ("balance_id") REFERENCES "balance" ("id");
 ALTER TABLE "community_goal" ADD CONSTRAINT "a4_community_goal_community_id_fk" FOREIGN KEY ("community_id") REFERENCES "community" ("id");
 ALTER TABLE "community_goal_meta" ADD CONSTRAINT "a4_community_meta_key_community_goal_id_fk" FOREIGN KEY ("community_goal_id") REFERENCES "community_goal" ("id");
-ALTER TABLE "community_goal_meta" ADD CONSTRAINT "a4_community_meta_key_community_goal_meta_key_key_fk" FOREIGN KEY ("key") REFERENCES "community_goal_meta_key" ("id");
-ALTER TABLE "balance" ADD CONSTRAINT "a4_balance_campaign_id_fk" FOREIGN KEY ("community_id") REFERENCES "community" ("id");
+ALTER TABLE "community_goal_meta" ADD CONSTRAINT "a4_community_meta_key_community_goal_meta_key_key_fk" FOREIGN KEY ("key") REFERENCES "community_goal_meta_key" ("key");
+ALTER TABLE "balance" ADD CONSTRAINT "a4_balance_community_id_fk" FOREIGN KEY ("community_id") REFERENCES "community" ("id");
 ALTER TABLE "community_association" ADD CONSTRAINT "a4_community_association_community_id_fk" FOREIGN KEY ("community_id") REFERENCES "community" ("id");
 ALTER TABLE "community_association" ADD CONSTRAINT "a4_community_association_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 ALTER TABLE "invitee" ADD CONSTRAINT "a4_invitee_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-ALTER TABLE "invitee" ADD CONSTRAINT "a4_invitee_community_id_fk" FOREIGN KEY ("campaign_id") REFERENCES "community" ("id");
+ALTER TABLE "invitee" ADD CONSTRAINT "a4_invitee_community_id_fk" FOREIGN KEY ("community_id") REFERENCES "community" ("id");
 ALTER TABLE "exchange" ADD CONSTRAINT "a4_exchange_debit_currency_id_fk" FOREIGN KEY ("debit_currency_id") REFERENCES "currency" ("id");
 ALTER TABLE "external_ledger" ADD CONSTRAINT "a4_external_ledger_fee_id_fk" FOREIGN KEY ("fee_id") REFERENCES "fee" ("id");
 ALTER TABLE "external_ledger" ADD CONSTRAINT "a4_external_ledger_currency_id" FOREIGN KEY ("currency_id") REFERENCES "currency" ("id");
@@ -265,3 +265,5 @@ ALTER TABLE "user_purchase" ADD CONSTRAINT "a4_user_purchase_user_id_fk" FOREIGN
 ALTER TABLE "user_purchase" ADD CONSTRAINT "a4_user_purchase_purchase_id_fk" FOREIGN KEY ("purchase_id") REFERENCES "purchase" ("id");
 ALTER TABLE "purchase" ADD CONSTRAINT "a4_purchase_refund_external_ledger_id_fk" FOREIGN KEY ("refund_ledger_id") REFERENCES "external_ledger" ("id");
 ALTER TABLE "purchase" ADD CONSTRAINT "a4_purchase_purchase_external_ledger_id_fk" FOREIGN KEY ("purchase_ledger_id") REFERENCES "external_ledger" ("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "a4_transaction_community_goal_id_fk" FOREIGN KEY ("community_goal_id") REFERENCES "community_goal" ("id");
+
