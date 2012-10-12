821b7ac8fb1623acb6869d407ae6422e431dbff3
Brian Oldfield <brian.oldfield@gmail.com>
2012-10-12 16:28:26 -0700

    Renamed enum type defs with '_enum'.

--- src/pooldb/sql/types.sql
+++ src/pooldb/sql/types.sql
@@ -1,4 +1,4 @@
-CREATE TYPE "balance_type" AS ENUM ('user', 'community');
-CREATE TYPE "community_goal_type" AS ENUM ('purchase', 'petition');
-CREATE TYPE "community_role_type" AS ENUM ('organizer', 'participant', 'invitee');
+CREATE TYPE "balance_type_enum" AS ENUM ('user', 'community');
+CREATE TYPE "community_goal_type_enum" AS ENUM ('purchase', 'petition');
+CREATE TYPE "community_role_enum" AS ENUM ('organizer', 'participant', 'invitee');
 CREATE TYPE "record_table_enum" AS ENUM ('transaction', 'exchange', 'transfer');
